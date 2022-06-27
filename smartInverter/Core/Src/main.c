ea55f3038cf6606ad900d3a2411d3f7ff54019c2
Chax97 <53842702+Chax97@users.noreply.github.com>
2022-06-27 20:23:11 +1000

    initial

--- smartInverter/Core/Src/main.c
+++ smartInverter/Core/Src/main.c
@@ -103,6 +103,7 @@ int main(void)
   HAL_TIM_Base_Start_IT(&htim1);
   HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
   HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+  //HAL_TIM_OC_Start(&htim1,TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_3);
   HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
   /* USER CODE END 2 */
